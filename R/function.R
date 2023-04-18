0d5d371052427f3c5525c3918c8239a9c968e504
NingWang0123 <1198137403wn@gmail.com>
2023-04-18 15:48:49 -0700

    updates

---
+++ R/function.R
@@ -0,0 +1,14 @@
+#' Split a string
+#'
+#' @param x A numeric number
+#' @param y A numeric number
+#'
+#' @return A numeric number.
+#' @export
+#'
+#' @examples
+#' add_function(1, 2)       # output: 3
+#' add_function(1,3)          # output: 4
+add_function<-function(x,y){
+  return(x+y)
+}
---
+++ tests/testthat/test-function.R
@@ -0,0 +1,3 @@
+test_that("addition works", {
+  expect_equal(add_function(2,2), 4)
+})
